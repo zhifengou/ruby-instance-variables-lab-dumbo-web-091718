671694ae7d6359440fe7f415aa801eeb7c24f676
zhifeng ou <zhifengou@hotmail.com>
2018-09-19 15:48:27 +0000

    automatically backed up by learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,9 +1,9 @@
 class Dog 
-  def name=(dog_name)
-    @this_dog_name=dog_name
+  def name=(dogs_name)
+    @this_dogs_name=dogs_name
   end
   def name
-    @this_dog_name
+    @this_dogs_name
   end
 end
 
