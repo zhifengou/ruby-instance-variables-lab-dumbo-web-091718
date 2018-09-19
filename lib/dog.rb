c46d6e0f6bf5f4d53d48bfb9979803628f41406c
zhifeng ou <zhifengou@hotmail.com>
2018-09-19 15:41:07 +0000

    automatically backed up by learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,8 +1,13 @@
 class Dog 
   def name=(dog_name)
-    this_dog_name=dog_name
+    @this_dog_name=dog_name
   end
   def name
-    this_dog_name
+    @this_dog_name
   end
 end
+
+lassie =Dog.new 
+lassie.name = "Lassie"
+
+puts Lassie.name
